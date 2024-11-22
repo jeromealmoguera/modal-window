a4a763018125bc5e583765b85c5a16fa4f84fdce
Jerome Almoguera <jeromealmoguera@gmail.com>
2024-11-22 12:04:07 +0800

    setup files and put querySelector in a variable

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// querySelector only select the first element with the same class
+// While querySelectorAll returns all element descendants of node that match selectors
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
