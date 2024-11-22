73c072b0153d0cb01182d4c31d22bf21d93c58f3
Jerome Almoguera <jeromealmoguera@gmail.com>
2024-11-22 15:38:52 +0800

    Add keyboard event for Escape key to also close the modal

--- script.js
+++ script.js
@@ -22,5 +22,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
