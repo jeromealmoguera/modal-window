726723eac5150bd8e7e4dbb3183baa903a06e14e
Jerome Almoguera <jeromealmoguera@gmail.com>
2024-11-22 14:48:41 +0800

    Add an eventListener click and remove to show and hide the modal

--- script.js
+++ script.js
@@ -8,7 +8,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 // While querySelectorAll returns all element descendants of node that match selectors
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
